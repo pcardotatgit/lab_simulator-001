f691fd02ee095ba854ff5b9dfb14178de0a80f0c
Patrick Cardot <patrick.cardot@gmail.com>
2022-12-05 08:42:16 +0100

    code update

--- code/config.py
+++ code/config.py
@@ -6,15 +6,15 @@
 ctr_client_id='paste_CTR_client_ID_here'
 ctr_client_password='paste_CTR_client_password_here'
 # For Europe
-#host = "https://private.intel.eu.amp.cisco.com" # For Europe
-#host_for_token="https://visibility.eu.amp.cisco.com" # For Europe
-# For the USE
-host = "https://private.intel.amp.cisco.com" # for US
-host_for_token="https://visibility.amp.cisco.com" # for the US
+host = "https://private.intel.eu.amp.cisco.com" # For Europe
+host_for_token="https://visibility.eu.amp.cisco.com" # For Europe
+# For the US
+#host = "https://private.intel.amp.cisco.com" # for US
+#host_for_token="https://visibility.amp.cisco.com" # for the US
 # For APJC 
 #host = "https://private.intel.apjc.amp.cisco.com" # For APJC
 #host_for_token="https://visibility.apjc.amp.cisco.com" # For APJC
 SecureX_Webhook_url=""
 webex_bot_token='' # 'token of your webex bot'
 webex_room_id='' #'room id of your alert webex team room'
-lab_date=""
+lab_date=""# for futur use
